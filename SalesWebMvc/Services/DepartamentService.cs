eb6d1920ad86501fc2beae19beaa96fb7e561c75
Rui Cavalcanti <sruicavalcanti@gmail.com>
2025-02-21 08:28:30 -0300

    Asyncrhonous operations using Task (async, await)

--- SalesWebMvc/Services/DepartamentService.cs
+++ SalesWebMvc/Services/DepartamentService.cs
@@ -1,6 +1,6 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,9 +15,9 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Departament> FindAll()
+        public async Task<List<Departament>> FindAllAsync()
         {
-          return  _context.Departament.OrderBy(x => x.Name).ToList();
+          return await _context.Departament.OrderBy(x => x.Name).ToListAsync();
         }
 
     }
